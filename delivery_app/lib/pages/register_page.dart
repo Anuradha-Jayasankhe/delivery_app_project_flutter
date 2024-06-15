9bbc71476c28add93c720d016587f771e3c29d78
Anuradha jayasanke <anuradhajekanayake@gmail.com>
2024-06-15 21:28:31 +0530

    6/15

--- delivery_app/lib/pages/register_page.dart
+++ delivery_app/lib/pages/register_page.dart
@@ -60,97 +60,99 @@ class _RegisterPageState extends State<RegisterPage> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //logo
-            Container(
-              height: 200,
-              width: 300,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                image: const DecorationImage(
-                  image: NetworkImage(
-                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtE-T-KluZZVKAMW0m6gxwxYIc6oiNJDC5717RbFe0mA&s'),
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-
-            //message,app slogan
-            Text(
-              "Let's create Account",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).colorScheme.inversePrimary,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-
-            //email textfeild
-            MyTextField(
-              controler: emailcontroller,
-              hinText: "Email",
-              abscureText: false,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            //password
-            MyTextField(
-              controler: passwordcontroller,
-              hinText: "Password",
-              abscureText: true,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            //confirm password
-            MyTextField(
-              controler: confirmpasswordcontroller,
-              hinText: "Confirm Password",
-              abscureText: true,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            //sign up button
-            MyButton(onTap: register, text: "Sign Up"),
-
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Already Have account",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.inversePrimary,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Container(
+                height: 200,
+                width: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtE-T-KluZZVKAMW0m6gxwxYIc6oiNJDC5717RbFe0mA&s'),
+                    fit: BoxFit.fill,
                   ),
                 ),
-                const SizedBox(
-                  width: 4,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+
+              //message,app slogan
+              Text(
+                "Let's create Account",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.inversePrimary,
                 ),
-                GestureDetector(
-                  onTap: widget.onTap,
-                  child: Text(
-                    "Login Now",
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+
+              //email textfeild
+              MyTextField(
+                controler: emailcontroller,
+                hinText: "Email",
+                abscureText: false,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              //password
+              MyTextField(
+                controler: passwordcontroller,
+                hinText: "Password",
+                abscureText: true,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              //confirm password
+              MyTextField(
+                controler: confirmpasswordcontroller,
+                hinText: "Confirm Password",
+                abscureText: true,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              //sign up button
+              MyButton(onTap: register, text: "Sign Up"),
+
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already Have account",
                     style: TextStyle(
-                        color: Theme.of(context).colorScheme.inversePrimary,
-                        fontWeight: FontWeight.bold),
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                    ),
                   ),
-                ),
-              ],
-            )
-            // not a member ,join now
-          ],
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  GestureDetector(
+                    onTap: widget.onTap,
+                    child: Text(
+                      "Login Now",
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.inversePrimary,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              )
+              // not a member ,join now
+            ],
+          ),
         ),
       ),
     );
